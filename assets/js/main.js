501598a5000b3b0c9d934f366d87e442593406dd
Marc Rodríguez <136878993+SirNadie@users.noreply.github.com>
2023-06-21 17:23:52 -0400

    nav responsive script

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            navc.classList.toggle('show')
+        })
+    }
+}
