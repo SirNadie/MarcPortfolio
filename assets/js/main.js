1c854fed445ded1380d37c87cb1221955246b1ae
Marc Rodríguez <136878993+SirNadie@users.noreply.github.com>
2023-07-01 10:18:18 -0400

    updatenavbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'))
+    navLink.forEach(n => n.classList.remove(''))
     this.classList.add('active')
 
     const navMenu = document.getElementById('nav-menu')
@@ -29,6 +29,7 @@ const sr = ScrollReveal({
 
 sr.reveal('.home__title', {})
 sr.reveal('.button', { delay: 200 })
+sr.reveal('.button1', { delay: 200 })
 sr.reveal('.home__img', { delay: 400 })
 sr.reveal('.home__social-icon', { interval: 200 })
 
@@ -38,8 +39,8 @@ sr.reveal('.about__text', { delay: 400 })
 
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__text', { delay: 200 })
-sr.reveal('.skills__data', {interval:200})
-sr.reveal('.skills__img', {delay:400})
+sr.reveal('.skills__data', { interval: 200 })
+sr.reveal('.skills__img', { delay: 400 })
 
-sr.reveal('.work__img', {interval:200})
-sr.reveal('.contact__input', {interval:200})
+sr.reveal('.work__img', { interval: 200 })
+sr.reveal('.contact__input', { interval: 200 })
